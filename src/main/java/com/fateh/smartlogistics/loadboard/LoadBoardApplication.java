d6635baba079b6ef0128ec661cb95493aefb29f4
Simr <snopes@tuta.io>
2024-05-07 15:40:40 +0000

    Init Repo

---
+++ src/main/java/com/fateh/smartlogistics/loadboard/LoadBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fateh.smartlogistics.loadboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fateh/smartlogistics/loadboard/LoadBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fateh.smartlogistics.loadboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
